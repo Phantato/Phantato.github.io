d89bc2acfe5f535c60052b8616c0015bb2a95ffa
Tan, Zhenghao <zhhtanjames@gmail.com>
2019-05-28 16:38:28 +0800

    modify category style

--- js/categoryAnime.js
+++ js/categoryAnime.js
@@ -1,5 +1,10 @@
 function showThisCategory(node)
 {
+    disp = node.getElementsByClassName("category-posts-show")[0]
+    if(disp){
+        disp.className = "catagory-posts"
+        return
+    }
     disp = document.getElementsByClassName("category-posts-show")[0]
     if(disp) disp.className = "category-posts"
     child = node.getElementsByClassName("category-posts")[0]
